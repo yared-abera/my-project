00745a433cfa033d2b36f6bd1a879356dd6dcd60
yared-abera <yaredoabera7@gmail.com>
2026-03-05 02:25:09 +0300

    contact  app creation

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
